4689c62f0fce28d7e2e96fa088108a4562dfd095
lokesh123d <dangwallokesh61@gmail.com>
2026-04-26 12:37:25 +0530

    add

--- useCustomeRedoUndo/src/hooks/useUndoRedo.jsx
+++ useCustomeRedoUndo/src/hooks/useUndoRedo.jsx
@@ -1,78 +1,78 @@
-import React, { useEffect, useState } from "react";
+  import React, { useEffect, useState } from "react";
 
-const useUndoRedo = (initialState) => {
-  const [value, setValue] = useState(initialState);
-  const [undoHistory, setUndoHistory] = useState([]);
-  const [historyRedo, setHistoryRedo] = useState([]);
-  console.log(value);
-  const [isTrue,setIsTrue] = useState(true);
+  const useUndoRedo = (initialState) => {
+    const [value, setValue] = useState(initialState);
+    const [undoHistory, setUndoHistory] = useState([]);
+    const [historyRedo, setHistoryRedo] = useState([]);
+    // console.log(value);
+    // const [isTrue,setIsTrue] = useState(true);
 
-  // const [history ,setHistory]
-  function undo() {
+    // const [history ,setHistory]
+    function undo() {
 
-    if (undoHistory.length == 0) return;
-     setIsTrue(false);
-    // setHistoryRedo((prev) => {
-    //   return [...prev, value];
-    // });
-    console.log(undoHistory);
-    console.log(undoHistory.length - 1);
-    setValue(undoHistory[undoHistory.length - 1]);
-   
-    setUndoHistory((prev) => {
-      const copy = [...prev];
-      copy.pop();
-      return copy;
-    });
-  }
-
-  function redo() {
-    if (historyRedo.length == 0) return;
-    setUndoHistory((prev) => {
-      return [...prev, value];
-    });
-    setValue(historyRedo[historyRedo.length - 1]);
-    setHistoryRedo((prev) => {
-      const copy = [...prev];
-      copy.pop();
-      return copy;
-    });
-  }
-
-  function reset() {
-    setHistoryRedo([]);
-    setUndoHistory([]);
-    setValue(initialState);
-  }
-
-  function canUndo() {
-    if (undoHistory.length > 0) {
-      console.log("ya You can ");
-      return true;
+      if (undoHistory.length == 0) return;
+      //  setIsTrue(false);
+      setHistoryRedo((prev) => {
+        return [...prev, value];
+      });
+      // console.log(undoHistory);
+      // console.log(undoHistory.length - 1);
+      setValue(undoHistory[undoHistory.length - 1]);
+    
+      setUndoHistory((prev) => {
+        const copy = [...prev];
+        copy.pop();
+        return copy;
+      });
     }
-    console.log("Nope");
-    return false;
-  }
 
-  function canRedo() {
-    if (historyRedo.length > 0) {
-      console.log("ya You can ");
-      return true;
-    }
-    console.log("Nope");
-    return false;
-  }
-
-  useEffect(() => {
-    if(isTrue) {
+    function redo() {
+      if (historyRedo.length == 0) return;
       setUndoHistory((prev) => {
         return [...prev, value];
       });
-      // setIsTrue(!isTrue)
+      setValue(historyRedo[historyRedo.length - 1]);
+      setHistoryRedo((prev) => {
+        const copy = [...prev];
+        copy.pop();
+        return copy;
+      });
     }
-  }, [value]);
 
-  return { undo, redo, value, setValue, reset, canRedo, canUndo };
-};
+    function reset() {
+      setHistoryRedo([]);
+      setUndoHistory([]);
+      setValue(initialState);
+    }
 
-export default useUndoRedo;
+    function canUndo() {
+      if (undoHistory.length > 0) {
+        console.log("ya You can ");
+        return true;
+      }
+      console.log("Nope");
+      return false;
+    }
+
+    function canRedo() {
+      if (historyRedo.length > 0) {
+        console.log("ya You can ");
+        return true;
+      }
+      console.log("Nope");
+      return false;
+    }
+
+    useEffect(() => {
+      // if(isTrue) {
+        setUndoHistory((prev) => {
+          return [...prev, value];
+        });
+        // setIsTrue(!isTrue)
+      // }
+    }, [value]);
+
+    return { undo, redo, value, setValue, reset, canRedo, canUndo };
+  };
+
+  export default useUndoRedo;
